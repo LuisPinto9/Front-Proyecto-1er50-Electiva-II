0fd8552d167899501679aacc2f8f9083d1552d21
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-12-16 13:40:15 -0500

    Arreglo de rutas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 
 require("dotenv").config();
 
@@ -7,16 +7,15 @@ const app = express();
 
 // setters
 app.set("PORT", process.env.PORT || 3001);
-app.set("views", path.join(__dirname, "/views"))
+app.set("views", path.join(__dirname, "/views"));
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use("/", require("./routes/index"))
-app.use("/add", require("./routes/index"))
+app.use("/", require("./routes/index"));
 
 app.listen(app.get("PORT"), () =>
   console.log(`Server listen at port ${app.get("PORT")}`)
