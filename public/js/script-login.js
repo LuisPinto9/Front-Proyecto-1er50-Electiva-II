69c1536ba1086f88a9958fe36295331a4da63a8d
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-12-16 15:04:27 -0500

    Arreglos de fornt

--- public/js/script-login.js
+++ public/js/script-login.js
@@ -5,42 +5,42 @@ document.getElementById("btnLogin").addEventListener("click", () => {
   const URI = "https://back-proyecto-1er50-electiva-ii.vercel.app/login/";
   const dataSend = {
     username: username,
-    password: password
+    password: password,
   };
   fetch(URI, {
     method: "POST",
     headers: {
-        "Content-Type": "application/json",
-      },
-    body: JSON.stringify(dataSend)
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataSend),
   })
     .then((response) => response.json())
     .then((data) => {
-        if(data.state){
-            console.log(data);
-            window.location.replace("/dashboard/home");
-            SaveLocalStorage("login",data.token)
-        }else{
-            alert(data.error)
-        }
+      if (data.state) {
+        console.log(data);
+        window.location.replace("/dashboard/home");
+        SaveLocalStorage("login", data.token);
+      } else {
+        alert(data.error);
+      }
     })
-    .catch(err=>{
-        alert(err)})
+    .catch((err) => {
+      alert(err);
+    });
 });
 
-
-const SaveLocalStorage =(key,item)=>{
-    //conseguir elementos en localStorage
-    let elementos = JSON.parse(localStorage.getItem(key))
-    //comprar si es un array
-    if(Array.isArray(elementos)){
-      elementos.push(item);
-    }else{
-      //crear array
-      elementos = item;
-    }
-    //guardar en el localStorage
-    localStorage.setItem(key, JSON.stringify([elementos]))
-    //devolver objeto
-    return item;
+const SaveLocalStorage = (key, item) => {
+  //conseguir elementos en localStorage
+  let elementos = localStorage.getItem(key);
+  //comprar si es un array
+  if (Array.isArray(elementos)) {
+    elementos.push(item);
+  } else {
+    //crear array
+    elementos = item;
   }
+  //guardar en el localStorage
+  localStorage.setItem(key, elementos);
+  //devolver objeto
+  return item;
+};
