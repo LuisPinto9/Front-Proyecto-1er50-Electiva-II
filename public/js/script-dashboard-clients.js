8141bfbd17b62818ac7e64cf782e5f24e19f2c54
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-12-16 15:06:49 -0500

    clienteCRUD

--- public/js/script-dashboard-clients.js
+++ public/js/script-dashboard-clients.js
@@ -5,7 +5,7 @@ const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI3NDU0OTQsImV4
 
 
 const apiUrl = "https://back-proyecto-1er50-electiva-ii.vercel.app/client";
-const authorizationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI3NDc2NTIsImV4cCI6MTcwMjgzNDA1Mn0.PVLGbu42cyVknj6rUen0_RY0QsW4dtFGu4zyf5nWm_Y";
+const authorizationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI3NTU2MTYsImV4cCI6MTcwMjg0MjAxNn0.deQpcFR6OMeYRxAHa9UD5vDTHZrN8BKQ4rdAIOFhlvc";
 
         fetch(apiUrl, {
             headers: {
@@ -20,38 +20,11 @@ const authorizationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI
             })
             .catch(error => console.error(error));
 
-// fetch(url, {
-//     method: 'GET', // o el método que necesites (GET, POST, etc.)
-//     headers: {
-//         'Authorization': `Bearer ${token}`
-//         // Agrega otros encabezados si es necesario
-//     }
-// })
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error => console.error('Error:', error));
-
-// fetch('https://back-proyecto-1er50-electiva-ii.vercel.app/client')
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log("aqui")
-//         console.log(data); 
-//         console.log("aqui")
-//     })
-//     .catch(error => console.error(error));
-
-// const loadTable = () => {
-//     console.log
-//     document.getElementById("table-body").innerHTML = "";
-//     return new Promise((resolve, reject) => {
-//       fetch("https://back-proyecto-1er50-electiva-ii.vercel.app/client")
-//         .then((datos) => datos.json())
-//         .then((datos) => {
 const loadTable = () => {
     document.getElementById("table-body").innerHTML = "";
     return new Promise((resolve, reject) => {
-        const apiUrl = "https://back-proyecto-1er50-electiva-ii.vercel.app/client";
-        const authorizationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI3NDc2NTIsImV4cCI6MTcwMjgzNDA1Mn0.PVLGbu42cyVknj6rUen0_RY0QsW4dtFGu4zyf5nWm_Y";
+        // const apiUrl = "https://back-proyecto-1er50-electiva-ii.vercel.app/client";
+        // const authorizationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDI3NDc2NTIsImV4cCI6MTcwMjgzNDA1Mn0.PVLGbu42cyVknj6rUen0_RY0QsW4dtFGu4zyf5nWm_Y";
 
         fetch(apiUrl, {
             headers: {
@@ -87,3 +60,65 @@ const loadTable = () => {
 };
 
  loadTable();
+ const findById = () => {
+    const option = document.getElementById("select-id");
+    if (option.value !== "Seleccione un ID") {
+        return new Promise((resolve, reject) => {
+            fetch(`https://back-proyecto-1er50-electiva-ii.vercel.app/client/${option.value}`, {
+                headers: {
+                    Authorization: `${authorizationToken}`
+                }
+            })
+            .then((data) => {
+                if (!data.ok) {
+                    throw new Error(`Error: ${data.status} - ${data.statusText}`);
+                }
+                return data.json();
+            })
+            .then((result) => {
+                const datos = result.data[0];
+                document.getElementById("table-body").innerHTML = "";
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${datos.id}</td>
+                    <td>${datos.name}</td>
+                    <td>${datos.celphone || "N/A"}</td>
+                    <td>${datos.email || "N/A"}</td>
+                    <td>${datos.email || "N/A"}</td>
+                    <td><button class='btn btn-danger' value='${datos.id}' onclick='drop(this.value)'>Eliminar</button></td>
+                `;
+
+                document.getElementById("table-body").appendChild(row);
+                console.log(row);
+            })
+            .catch((error) => reject(error));
+        });
+    }
+};
+
+
+ const drop = (id) => {
+    const URI = `https://back-proyecto-1er50-electiva-ii.vercel.app/client/${id}`;
+    fetch(URI, {
+      method: "DELETE",
+      headers: {
+        Authorization: `${authorizationToken}`
+      }
+    })
+    .then((response) => response.json())
+      .then((result) => {
+        if (result.state) {
+          loadTable();
+          alert("Cliente eliminado");
+        } else {
+          alert("Error al eliminar el cliente");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Ocurrió un error al eliminar el cliente");
+      });
+  };
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,11 @@ router.get("/login", (req, res) =>
 router.get("/dashboard/home", (req, res) =>
   res.sendFile(path.join(__dirname + "../../views/dashboard-home.html"))
 );
+ router.get("/tabla", (req, res)=> 
+ res.sendFile(path.join(__dirname + "../../views/dashboard-clients.html")))
+
 router.get("/dashboard/clientes", (req, res) =>
-  res.sendFile(path.join(__dirname + "../../views/dashboard-clients.html"))
+res.sendFile(path.join(__dirname + "../../views/dashboard-clients.html"))
 );
 router.get("/dashboard/reservaciones", (req, res) =>
   res.sendFile(path.join(__dirname + "../../views/dashboard-reservations.html"))
